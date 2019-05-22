aca662f35af9b3cd439b1e62cea042d13c5d1c1b
talhanousher <talhanousher@gmail.com>
2019-05-22 20:59:34 +0500

    added alien row to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,14 +14,15 @@ def run_game():
         (game_settings.screen_width, game_settings.scree_height))
     pygame.display.set_caption(game_settings.game_caption)
     ship = Ship(game_settings, screen)
-    alien = Alien(game_settings, screen)
+    aliens = Group()
     bullets = Group()
+    game_functions.create_fleet(game_settings, screen, aliens)
     while True:
         game_functions.check_events(game_settings, screen, ship, bullets)
         ship.update()
         game_functions.update_bullets(bullets)
         game_functions.update_screen(
-            game_settings, screen, ship, alien, bullets)
+            game_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 
 def check_events(game_settings, screen, ship, bullets):
@@ -14,12 +15,12 @@ def check_events(game_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(game_settings, screen, ship, alien, bullets):
+def update_screen(game_settings, screen, ship, aliens, bullets):
     screen.fill(game_settings.background_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
     pygame.display.flip()
 
 
@@ -52,3 +53,15 @@ def fire_bullets(game_settings, screen, ship, bullets):
     if len(bullets) < game_settings.bullets_allowed:
         new_bullet = Bullet(game_settings, screen, ship)
         bullets.add(new_bullet)
+
+
+def create_fleet(game_settings, screen, aliens):
+    alien = Alien(game_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = game_settings.screen_width - alien_width
+    number_aliens_x = int(available_space_x/(2*alien_width))
+    for alien_number in range(number_aliens_x):
+        alien = Alien(game_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
